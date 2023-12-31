d9dbcc1dbf1bdd79577a0705856d9caa52848867
Francesc Alberola <f.alberola@gmail.com>
2023-12-31 07:22:13 +0100

    ordenacion ok

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductInterface } from '../../interfaces/product-interface';
-import { ProductsService } from '../../services/products.service';
 import { CommonModule } from '@angular/common';
-import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTableModule, NzTableSortOrder } from 'ng-zorro-antd/table';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -13,13 +13,13 @@ import { NzTableModule } from 'ng-zorro-antd/table';
 })
 export class ProductsComponent implements OnInit {
   public products: Array<ProductInterface> = [];
-  
+
   constructor(private productService: ProductsService) {}
-   
+
   ngOnInit(): void {
     this.getProducts();
   }
-  
+
   getProducts() {
     this.productService.getProducts().subscribe({
       next: (data) => {
@@ -29,5 +29,39 @@ export class ProductsComponent implements OnInit {
         console.error('Error fetching products:', error);
       }
     });
+  }
+
+  sortTitle(sort: string | null): void {
+    if (sort !== null) {
+      this.products = this.sortData(sort, this.products, (item: ProductInterface) => item.title);
+    }
+  }
+
+  sortCategory(sort: string | null): void {
+    if (sort !== null) {
+      this.products = this.sortData(sort, this.products, (item: ProductInterface) => item.category);
+    }
+  }
+
+  sortPrice(sort: string | null): void {
+    if (sort !== null) {
+      this.products = this.sortData(sort, this.products, (item: ProductInterface) => item.price);
+    }
+  }
+
+  private sortData(sort: string, data: ProductInterface[], extractKey: (item: ProductInterface) => any): ProductInterface[] {
+    const sortOrder: NzTableSortOrder = sort as NzTableSortOrder;
+    return [...data.sort((a, b) => {
+      const keyA = extractKey(a);
+      const keyB = extractKey(b);
+
+      if (sortOrder === 'ascend') {
+        return keyA > keyB ? 1 : -1;
+      } else if (sortOrder === 'descend') {
+        return keyA < keyB ? 1 : -1;
+      } else {
+        return 0;
+      }
+    })];
   }
 }
